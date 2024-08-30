86b9222cfeb7fb720bbd68d8011ab60f31c18406
Arthur <arthurregiani@gmail.com>
2024-08-30 19:20:12 -0300

    Modificado tratamento de permissões

--- app/src/main/java/br/edu/ifsp/dmo/whatsapp/utils/PermissionManager.kt
+++ app/src/main/java/br/edu/ifsp/dmo/whatsapp/utils/PermissionManager.kt
@@ -1,7 +1,6 @@
 package br.edu.ifsp.dmo.whatsapp.utils
 
 import android.app.Activity
-import android.content.DialogInterface
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
