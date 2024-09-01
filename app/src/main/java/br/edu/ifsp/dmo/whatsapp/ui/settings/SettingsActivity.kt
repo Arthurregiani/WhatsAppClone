87676cf84dd29e3288e2c26d289ec879be923cd6
Arthur <arthurregiani@gmail.com>
2024-09-01 15:36:12 -0300

    Implementado adicionar foto de perfil da galeria ou camera

--- app/src/main/java/br/edu/ifsp/dmo/whatsapp/ui/settings/SettingsActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/whatsapp/ui/settings/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package br.edu.ifsp.dmo.whatsapp.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.dmo.whatsapp.R
@@ -21,6 +23,11 @@ class SettingsActivity : AppCompatActivity() {
         android.Manifest.permission.CAMERA
     )
 
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 100
+        const val REQUEST_IMAGE_PICK = 200
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityConfiguracoesBinding.inflate(layoutInflater)
@@ -28,9 +35,49 @@ class SettingsActivity : AppCompatActivity() {
         permissionManager = PermissionManager(this)
 
         requestPermissions()
-        configureImageProfile()
         configureToolbar()
+        configureImageProfile()
+        configureButtons()
     }
+
+    private fun configureButtons() {
+        binding.imageButtonGalery.setOnClickListener {
+            val galeryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galeryIntent, REQUEST_IMAGE_PICK)
+
+        }
+
+        binding.imageButtonCam.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (cameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            var imageBitmap: android.graphics.Bitmap? = null
+            when (requestCode) {
+                REQUEST_IMAGE_PICK -> {
+                    val selectedImageUri = data?.data
+                    imageBitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImageUri)
+                }
+
+                REQUEST_IMAGE_CAPTURE -> {
+                    imageBitmap = data?.extras?.get("data") as android.graphics.Bitmap
+                }
+            }
+            if (imageBitmap != null) {
+                binding.profileImage.setImageBitmap(imageBitmap)
+            }
+
+        }
+
+    }
+
 
     // Solicita as permissões
     private fun requestPermissions() {
@@ -49,16 +96,19 @@ class SettingsActivity : AppCompatActivity() {
 
 
     private fun configureImageProfile() {
-        val imageUrl = "" // URL da imagem
-        val placeholder = R.drawable.user_image_default // Imagem padrão enquanto carrega
 
-        Glide.with(this)
-            .load(imageUrl)
-            .apply(RequestOptions()
-                .placeholder(placeholder) // Exibe imagem padrão enquanto carrega
-                .circleCrop() // Aplica o formato circular
-            )
-            .into(binding.profileImage)
+
+//        val imageUrl = "" // URL da imagem
+//        val placeholder = R.drawable.user_image_default // Imagem padrão enquanto carrega
+//
+//        Glide.with(this)
+//            .load(imageUrl)
+//            .apply(
+//                RequestOptions()
+//                    .placeholder(placeholder) // Exibe imagem padrão enquanto carrega
+//                    .circleCrop() // Aplica o formato circular
+//            )
+//            .into(binding.profileImage)
     }
 
     private fun configureToolbar() {
